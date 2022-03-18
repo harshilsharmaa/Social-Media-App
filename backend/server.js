8ba67636c1e6d8be89ce8821e6e33bafabf75eff
harshil sharma <harshilsharmaa0@gmail.com>
2022-03-18 21:21:25 +0530

    Second Commit

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_SECRET
 })
 
+const PORT = process.env.PORT || 4000;
 
-app.listen(process.env.PORT, () => {
-    console.log("Server is running on port: 4000"  );
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`  );
 });
